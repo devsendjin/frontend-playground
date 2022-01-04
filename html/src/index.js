85197cbc351dbf4a4997faac66080c59c3cb208f
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2022-01-05 00:39:29 +0200

    Ready html playground

---
+++ html/src/index.js
@@ -0,0 +1,7 @@
+import './index.scss';
+
+console.log('index');
+
+function some() {
+  console.log('index');
+}
---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  requirePragma: false,
+  proseWrap: 'preserve',
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 120,
+  // below line only for windows users facing CLRF and eslint/prettier error
+  // non windows users feel free to delete it
+  endOfLine: 'auto',
+  arrowParens: 'always',
+};
