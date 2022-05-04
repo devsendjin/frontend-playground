c5e9f50b2a5a0cbe2ff5d7cd2414d7d5634e4943
AnatoliySkichko <anatoliy.skichko.dev@gmail.com>
2022-05-04 19:12:50 +0300

    react: updated ScaffoldComponent

--- packages/react/src/scaffoldings/ScaffoldComponent/ScaffoldComponent.tsx
+++ packages/react/src/scaffoldings/ScaffoldComponent/ScaffoldComponent.tsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import cn from 'classnames';
 import styles from './ScaffoldComponent.module.scss';
 
-const ScaffoldComponent: RFC = () => {
-  return <div className={styles['scaffold']}>ScaffoldComponent</div>;
+interface IScaffoldComponentProps {
+  className?: string;
+}
+
+const ScaffoldComponent: React.FC<IScaffoldComponentProps> = ({ className }) => {
+  return <div className={cn(styles['scaffold'], className)}>ScaffoldComponent</div>;
 };
 
 export { ScaffoldComponent };
