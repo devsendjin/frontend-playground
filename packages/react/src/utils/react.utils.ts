b5aa20855d88620d061db0364250b04132a2623f
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2022-11-17 03:43:59 +0200

    react: added util responsive for merging refs

---
+++ packages/react/src/utils/react.utils.ts
@@ -0,0 +1,16 @@
+/**
+ * @description React utility to merge refs 🖇
+ */
+export function mergeRefs<T = any>(
+  refs: Array<React.MutableRefObject<T> | React.LegacyRef<T>>
+): React.RefCallback<T> {
+  return (value) => {
+    refs.forEach((ref) => {
+      if (typeof ref === "function") {
+        ref(value);
+      } else if (ref != null) {
+        (ref as React.MutableRefObject<T | null>).current = value;
+      }
+    });
+  };
+}
