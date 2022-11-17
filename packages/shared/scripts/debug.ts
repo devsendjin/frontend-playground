93a361fc1a3ab67dff4d7a017e137b0be072ea24
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2022-11-17 03:39:56 +0200

    shared: updated mixins.scss & types

--- packages/shared/scripts/debug.ts
+++ packages/shared/scripts/debug.ts
@@ -1,6 +1,6 @@
-type TOutline = () => void;
+type OutlineFunc = () => void;
 
-const outline: TOutline = () => {
+const outline: OutlineFunc = () => {
   Array.from(document.querySelectorAll('*')).forEach(function (el) {
     (el as HTMLElement).style.outline = '1px solid #' + (~~(Math.random() * (1 << 24))).toString(16);
   });
