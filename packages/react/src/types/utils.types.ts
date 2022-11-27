c9d9dfd668268bf37c49bc4e0ca98b77c5503c9f
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2022-11-27 23:25:38 +0200

    react: added TS type utils

--- packages/react/src/types/utils.types.ts
+++ packages/react/src/types/utils.types.ts
@@ -5,3 +5,6 @@ export type Primitives = number | string | boolean | bigint | Negatives;
 export type ObjectValues<T> = T[keyof T];
 
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+// React relative
+export type ExtractComponent<Type> = Type extends React.FC<infer X> ? X : never;
--- packages/react/src/utils/type.utils.ts
+++ packages/react/src/utils/type.utils.ts
@@ -3,3 +3,5 @@ export const isBoolean = (value: unknown): value is boolean => typeof value === 
 export const isString = (value: unknown): value is string => typeof value === "string";
 
 export const isNumber = (value: unknown): value is number => typeof value === "number";
+
+export const isNull = (value: unknown): value is null => value === null;
