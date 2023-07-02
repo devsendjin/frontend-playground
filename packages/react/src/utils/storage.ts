1ce3de99b503b271fcd2b117ed5216b121e0c068
Anatoliy.Skichko <anatoliy.skichko.dev@gmail.com>
2023-07-03 02:55:01 +0300

    react: added "LocalStorageManager" class util

--- packages/react/src/utils/storage.ts
+++ packages/react/src/utils/storage.ts
@@ -1,223 +1,94 @@
-import { isNull, isString } from "./types.utils";
+import { isBoolean, isNumber, isString } from "./types.utils";
 
-type IWebStorage = {
-  get(key: string): void;
+const isLSObject = (value: unknown): value is LSObject =>
+  value != null && typeof value === "object" && !Array.isArray(value);
+const isLSPrimitive = (value: unknown): value is LSPrimitive => isNumber(value) || isString(value) || isBoolean(value);
 
-  set(key: string, payload: any): void;
-
-  remove(key: string): void;
-
-  forceClear(): void;
-
-  update<ExpectedValueT>(key: string, callback: (param: ExpectedValueT) => ExpectedValueT): void;
-
-  softClear(): void;
-
-  addToExcludes(keys: string[]): void;
-};
-
-enum QUOTA_EXCEEDED_ERROR_STATUS_CODES {
-  CHROME = 22,
-  MOZILLA = 1014,
-  SAFARI = 22,
-  EDGE = 22,
-  IE = 22,
-}
-
-class WebStorage implements IWebStorage {
-  private readonly usedSpace: number;
-  private readonly excludeList: string[] = [];
-
-  constructor() {
-    this.usedSpace = this.checkUsedSpace();
-  }
-
-  public get = <RT = string>(key: string): RT => {
-    const payload = localStorage.getItem(key);
-
-    if (!payload) throw Error(`Can not find any values by key ${key}`);
-
-    let res;
-    try {
-      res = JSON.parse(payload);
-    } catch (e) {
-      res = payload;
-    }
-
-    return res;
-  };
-
-  public set = (key: string, payload: any): void => {
-    if (!payload || !Object.keys(payload).length) return;
-    try {
-      localStorage.setItem(key, JSON.stringify(payload));
-    } catch (e) {
-      const error: any = e;
-      if (this.matchToQuotaExceedError(error.code)) {
-        console.log("Clear LS");
-        this.softClear();
-        localStorage.setItem(key, JSON.stringify(payload));
-        return;
-      }
-      console.error(e);
-    }
-  };
-
-  public remove = (key: string) => localStorage.removeItem(key);
-
-  public update = <ExpectedValueT>(key: string, callback: (param: ExpectedValueT) => ExpectedValueT): void => {
-    const value = this.get<ExpectedValueT>(key);
-    if (!value) throw Error(`Can not find any values by key ${key}`);
-
-    const newValue = callback(value);
-    this.set(key, newValue);
-  };
-
-  public forceClear = () => localStorage.clear();
-
-  public addToExcludes = (keys: string[]) => keys.forEach((k) => this.excludeList.push(k));
-
-  /**
-   * @description clear all LS values except exclude list items
-   */
-  public softClear = (): void => {
-    const archive: [[string, string]] = [] as unknown as [[string, string]];
-
-    for (let i = 0; i < this.excludeList.length; i++) {
-      const item = this.excludeList[i];
-      const result = localStorage.getItem(`${item}`);
-      if (!result) continue;
-      archive.push([item, result]);
-    }
-
-    this.forceClear();
-
-    archive.forEach(([key, value]) => this.set(key, value));
-  };
-
-  private checkUsedSpace(): number {
-    return Object.keys(window.localStorage)
-      .map((key) => {
-        return localStorage[key].length;
-      })
-      .reduce((a, b) => {
-        return a + b;
-      }, 0);
-  }
-
-  private matchToQuotaExceedError = (code: number): boolean => {
-    let status: boolean = false;
-
-    for (const [_, enumValue] of Object.entries(QUOTA_EXCEEDED_ERROR_STATUS_CODES)) {
-      if (enumValue === code) {
-        status = true;
-        break;
-      }
-    }
-    return status;
-  };
-
-  get getUsedSpace(): number {
-    return this.usedSpace;
-  }
-
-  get getExcludeList(): string[] {
-    return this.excludeList;
-  }
-}
-
-export const LS = new WebStorage();
-
-type LSPrimitives = number | string | boolean;
+type LSPrimitive = number | string | boolean;
 type LSPrimitiveKey = string | number;
-type LSObject = Record<LSPrimitiveKey, any>;
+type LSObject = Record<LSPrimitiveKey, unknown>;
 
-const isLSPrimitive = (value: unknown): value is string => isString(value) && (!value.startsWith("{") && !value.startsWith("["));
+export class LocalStorageManager<Data extends LSPrimitive | LSPrimitive[] | LSObject | LSObject[]> {
+  constructor(private readonly namespace: string) {}
 
-export class LocalStorageManager<
-  Data extends LSObject | LSPrimitives,
-  Entity = Data extends LSObject ? keyof Data : never
-> {
-  constructor(private readonly storageKey: string) {}
+  public set<
+    KeyOrValue extends Data extends LSObject ? keyof Data : Data,
+    Value extends Data extends LSObject ? Data[keyof Data] : never
+  >(...args: Data extends LSObject ? [KeyOrValue, Value] : [Data]): void {
+    const data = this.parse();
 
-  public get(entity: Entity): Data | null {
-    const item = localStorage.getItem(this.storageKey);
-    if (!item) return null;
+    // primitive
+    if (args.length === 1) {
+      const value = args[0];
+      this.persist(value);
+      return;
+    }
+
+    // object
+    if (args.length === 2) {
+      const key = args[0] as keyof Data;
+      const value = args[1];
+      const _data = data ?? {};
+      const newObject = { ..._data, [key]: value };
+      this.persist(newObject);
+    }
+  }
+
+  public get<
+    Key extends Data extends LSObject ? keyof Data : never,
+    Value extends Data extends LSObject ? Data[keyof Data] : Data
+  >(...args: Data extends LSObject ? [Key] : never): Value | null {
+    const data = this.parse();
+    if (isLSObject(data)) {
+      const key = args[0];
+      return (data[key] as Value) ?? null;
+    }
+    return (data as Value) ?? null;
+  }
+
+  public remove(...args: Data extends LSObject ? [keyof Data] : never): void {
+    const data = this.parse();
+    if (isLSObject(data)) {
+      const key = args[0];
+      delete data[key];
+      this.persist(data);
+      return;
+    }
+    this.clear();
+  }
+
+  public clear(): void {
+    localStorage.removeItem(this.namespace);
+  }
+
+  private persist<Value extends LSPrimitive | LSPrimitive[] | LSObject | LSObject[]>(value: Value): void {
+    const stringifiedValue = isLSPrimitive(value) ? String(value) : JSON.stringify(value);
+    localStorage.setItem(this.namespace, stringifiedValue);
+  }
+
+  private parse(): Data | null {
+    const namespaceValue = localStorage.getItem(this.namespace);
+    if (namespaceValue === null) return null;
     try {
-      if (!isLSPrimitive(item)){
-        const parsed = JSON.parse(item);
-        return parsed[entity];
-      }
-      return item as Data;
-    } catch (e) {
-      console.error(e);
+      return JSON.parse(namespaceValue);
+    } catch (error) {
       return null;
     }
-  }
-
-  public getAll(): Data | null {
-    const item = localStorage.getItem(this.storageKey);
-    if (!item) return null;
-    try {
-      if (!isLSPrimitive(item)){
-        return JSON.parse(item) as Data;
-      }
-      return item as Data;
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
-  }
-
-  public set(entity: Entity, value: Data extends LSObject ? Data[keyof Data] : never): this {
-    try {
-      const savedData = this.getAll();
-
-      if (!isLSPrimitive(savedData)) {
-        localStorage.setItem(this.storageKey, JSON.stringify({[entity as unknown as keyof Data]: value}));
-      }
-    } catch (e) {
-      console.error(e);
-    } finally {
-      return this;
-    }
-  }
-
-  public override(value: Data): this {
-    try {
-      const dataToSet = isLSPrimitive(value) ? String.prototype.toString.call(value) : JSON.stringify(value);
-      localStorage.setItem(this.storageKey, dataToSet);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      return this;
-    }
-  }
-
-  public remove(...entities: Entity[]): this {
-    try {
-      const savedData = this.getAll();
-      if (!isNull(savedData) && !isLSPrimitive(savedData) && entities.length) {
-        entities.forEach(entity => {
-          if (entity in savedData) {
-            delete savedData[entity as unknown as keyof Data];
-          }
-        })
-        localStorage.setItem(this.storageKey, JSON.stringify(savedData));
-      }
-    } catch (e) {
-      console.error(e);
-    } finally {
-      return this;
-    }
-  }
-
-  public has(entity: Entity): boolean {
-    return !!this.get(entity);
-  }
-
-  public clear(): this {
-    localStorage.removeItem(this.storageKey);
-    return this;
   }
 }
+
+// const localStorageManager1 = new LocalStorageManager<{ a: number | null }>("testNamespace");
+// localStorageManager1.set("a", null);
+// localStorageManager1.get("a");
+
+// const localStorageManager2 = new LocalStorageManager<string[]>("testNamespace1");
+// localStorageManager2.set(["a"]);
+// localStorageManager2.get();
+
+// const localStorageManager3 = new LocalStorageManager<number[]>("testNamespace3");
+// localStorageManager3.set([1, 2, 3]);
+// localStorageManager3.get();
+
+// const localStorageManager4 = new LocalStorageManager<number>("testNamespace4");
+// localStorageManager4.set(3);
+// localStorageManager4.get();
