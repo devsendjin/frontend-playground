4fb594bba72bf94be656c29538314bde2d642a9e
Anatoliy.Skichko <anatoliy.skichko.dev@gmail.com>
2023-09-04 19:31:58 +0300

    react: context.tsx naming

--- packages/react/src/scaffoldings/context.tsx
+++ packages/react/src/scaffoldings/context.tsx
@@ -1,16 +1,16 @@
 import { createContext, useContext } from 'react';
 
-type TScaffoldContext = {};
+type ScaffoldContextType = {};
 
-const ScaffoldContext = createContext(null as unknown as TScaffoldContext);
+const ScaffoldContext = createContext(null as unknown as ScaffoldContextType);
 ScaffoldContext.displayName = 'ScaffoldContext';
 
-const ScaffoldProvider: RFC<{ value: TScaffoldContext }> = ({ value, children }) => {
+const ScaffoldProvider: RFC<{ value: ScaffoldContextType }> = ({ value, children }) => {
   return <ScaffoldContext.Provider value={value}>{children}</ScaffoldContext.Provider>;
 };
 ScaffoldProvider.displayName = ScaffoldProvider.name;
 
-const useScaffoldContext = (): TScaffoldContext => {
+const useScaffoldContext = (): ScaffoldContextType => {
   const context = useContext(ScaffoldContext);
   if (!context) {
     throw new Error('No context found for Scaffold');
